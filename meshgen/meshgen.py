f5054460727d3e513a03852cea33313e4a1aca1a
Eivind Fonn <evfonn@gmail.com>
2015-03-05 16:20:34 +0100

    Document meshgen.py

--- meshgen/meshgen.py
+++ meshgen/meshgen.py
@@ -18,10 +18,12 @@ from volumetricslice import VolumetricSlice
 
 
 def fill_runner(af):
+    """Produces slices from airfoils. Used for parallell computation."""
     return Slice(af)
 
 
 def progress(title, i, tot):
+    """Progress output to stdout."""
     args = (title, i, tot, 100 * float(i) / tot)
     s = '\r%s: %i/%i (%.2f%%)' % args
     if i == tot:
@@ -31,66 +33,74 @@ def progress(title, i, tot):
 
 
 class MeshGen(object):
+    """This is a utility class used for mesh generation. It will not do everything on its
+    own. Methods must be called in the right order. Calling code can rely on just checking mesh_mode
+    and length_mode."""
 
     def __init__(self, params):
         self.p = params
 
-
     def load_airfoils(self):
+        """Loads airfoils from the wing definition file."""
         self.airfoils = [AirFoil.from_wingdef(self.p, wd) for wd in self.p.wingdef]
         self.p.dump_g2files('airfoils_raw', self.airfoils)
 
-
     def resample_airfoils(self):
+        """Performs angular resampling of all airfoils."""
         for af in self.airfoils:
             af.resample()
-        self.p.dump_g2files('airfoils_resampled_radial', self.airfoils)
-
+        self.p.dump_g2files('airfoils_resampled_angular', self.airfoils)
 
     def resolve_join(self):
+        """Resolves the join according to the join_adds and join_index parameters."""
         n = self.p.join_adds
         idx = self.p.join_index
 
         if n == 0:
             return
 
+        # Interpolation upwards
         for _ in xrange(n):
             new = AirFoil.from_mean(self.airfoils[idx], self.airfoils[idx+1])
             self.airfoils.insert(idx + 1, new)
 
+        # Interpolation downwards
         for i in xrange(n):
             new = AirFoil.from_mean(self.airfoils[idx+i-1], self.airfoils[idx+i])
             self.airfoils.insert(idx + i, new)
 
+        # Remove the join
         del self.airfoils[idx + n]
+
         self.p.dump_g2files('airfoils_joined', self.airfoils)
 
-
     def resample_length(self):
+        """Resamples airfoils in the length direction."""
+        # Loft the curves to produce a wing surface
         curves = [af.curve for af in self.airfoils]
         zvals = [af.z() for af in self.airfoils]
         wing = LoftCurves(curves, zvals, order=4)
+
+        # Interpolating curve for theta as a function of z
         thetas = ip.CubicCurve(x=[af.theta for af in self.airfoils],
                                t=zvals, boundary=ip.NATURAL)
         theta = lambda z: thetas.Evaluate(z)[0]
 
-        if self.p.length_mode == 'uniform':
-            zvals = self._resample_length_uniform(zvals[0], zvals[-1])
-        elif self.p.length_mode == 'double':
-            zvals = self._resample_length_double(zvals[0], zvals[-1])
-        elif self.p.length_mode == 'triple':
-            zvals = self._resample_length_triple(zvals[0], zvals[-1])
+        # Get the resampled z-values
+        resampler = getattr(self, '_resample_length_' + self.p.length_mode)
+        new_zvals = resampler(zvals[0], zvals[-1])
 
+        # Produce new airfoils from point-evaluating the wing
         self.airfoils = []
         kus, _ = wing.GetKnots()
-        for z in zvals:
+        for z in new_zvals:
             pts = [wing.Evaluate(ku, z) for ku in kus]
             self.airfoils.append(AirFoil.from_pts(self.p, theta(z), pts))
 
         self.p.dump_g2files('airfoils_resampled_length', self.airfoils)
 
-
     def make_slices(self):
+        """Turns airfoils into slices."""
         progress('Making slices', 0, len(self.airfoils))
         pool = mp.Pool(self.p.nprocs_mg)
         result = []
@@ -102,27 +112,27 @@ class MeshGen(object):
         self.slices = result
         self.p.dump_g2files('slices_raw', self.slices)
 
-
     def extend(self):
+        """Extend all the slices according to parameters (behind, ahead, sides)."""
         for s in self.slices:
             s.extend()
         self.p.dump_g2files('slices_sides', self.slices)
 
-
     def extrude(self):
+        """Performs extrusion of a single airfoil or slice."""
         zvals = np.linspace(0, self.p.length, self.p.n_length + 1)
         attr = 'slices' if hasattr(self, 'slices') else 'airfoils'
         root = getattr(self, attr)[0]
         setattr(self, attr, [root.translate(Point(0,0,z)) for z in zvals])
 
-
     def loft_slices(self):
+        """Lofts slices together to produce volumetric slices."""
         temp = VolumetricSlice.from_slices(self.slices)
         self.slices = temp.subdivide()
         self.p.dump_g2files('slices_volumetric', self.slices)
 
-
     def loft_blade(self):
+        """Lofts airfoils together to produce a blade."""
         params = extend_knots(map(float, range(len(self.airfoils))))
         airfoils = list(self.airfoils)
         airfoils.insert(1, AirFoil.from_mean(airfoils[0], airfoils[1]))
@@ -130,14 +140,14 @@ class MeshGen(object):
 
         self.blade = LoftCurves([af.curve for af in airfoils], params, 4)
 
-
     def subdivide_slices(self):
+        """Subdivices all the slices."""
         for s in self.slices:
             s.subdivide()
         self.p.dump_g2files('slices_subdivided', self.slices)
 
-
     def lower_order(self):
+        """Lowers order on the slices and/or the blade."""
         if hasattr(self, 'slices'):
             for s in self.slices:
                 s.lower_order()
@@ -145,47 +155,53 @@ class MeshGen(object):
         if hasattr(self, 'blade'):
             lower_order(self.blade, self.p.order)
 
-
     def output(self):
+        """Produces the final output."""
         if self.p.format == 'OpenFOAM' and self.p.mesh_mode in {'2d', 'semi3d'}:
             self.slices = [VolumetricSlice.from_slice(s) for s in self.slices]
 
         getattr(self, '_output_' + self.p.mesh_mode)()
         self.p.out_yaml()
 
-
     def _output_blade(self):
+        """Produces the final output of the blade."""
         path = abspath(join(self.p.out, self.p.out)) + '.g2'
         WriteG2(path, self.blade)
 
-
     def _output_2d(self):
+        """Produces the final output in 2D mode."""
         path = abspath(join(self.p.out, self.p.out))
         self.slices[0].output(path)
 
-
     def _output_semi3d(self):
+        """Produces the final output in semi3D mode."""
         progress('Writing planes', 0, len(self.slices))
         for i, s in enumerate(self.slices):
             path = abspath(join(self.p.out, 'slice-%03i' % (i+1)))
+
+            # In OpenFOAM mode, we have to output to separate subfolders
             if self.p.format == 'OpenFOAM':
                 try:
                     os.makedirs(path)
                 except OSError:
                     pass
                 path = join(path, 'out')
+
             s.output(path)
             progress('Writing planes', i+1, len(self.slices))
 
-        zvals = [s.z() for s in self.slices]
-        beam = ip.LinearCurve(pts=[Point(z,0,0) for z in zvals])
-        WriteG2(join(self.p.out, 'beam.g2'), beam)
-
+        # Output the beam if IFEM mode.
+        if self.p.format == 'IFEM':
+            zvals = [s.z() for s in self.slices]
+            beam = ip.LinearCurve(pts=[Point(z,0,0) for z in zvals])
+            WriteG2(join(self.p.out, 'beam.g2'), beam)
 
     def _output_3d(self):
+        """Produces the final output in 3D mode."""
         path = abspath(join(self.p.out, self.p.out))
         n = Numberer()
 
+        # Add all the patches and boundaries
         for s in self.slices:
             s.push_patches(n)
             s.push_boundaries(n)
@@ -198,18 +214,19 @@ class MeshGen(object):
         if self.p.walldistance:
             n.AddWallGroup('wing')
 
+        # Renumber and final output
         n.Renumber(self.p.nprocs)
         n.WriteEverything(path, display=True)
 
         if self.p.format == 'OpenFOAM':
             convert_openfoam(path)
 
-
     def _resample_length_uniform(self, za, zb):
+        """Uniform length resampling."""
         return np.linspace(za, zb, self.p.n_length + 1)
 
-
     def _resample_length_double(self, za, zb):
+        """Double-sided length resampling."""
         n = self.p.n_length / 2
         dj, dt = self.p.d_join, self.p.d_tip
 
@@ -219,8 +236,8 @@ class MeshGen(object):
         zvals += gradspace(zb, -dt, r2, n)[::-1]
         return zvals
 
-
     def _resample_length_triple(self, za, zb):
+        """Triple-sided length resampling."""
         join = self.p.wingdef[self.p.join_index].z
         dj, dt = self.p.d_join, self.p.d_tip
         nb, nl = self.p.n_base, (self.p.n_length - self.p.n_base) / 2

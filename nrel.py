7f448667c5748d81224c15eae50ff3e4aef91560
Timo van Opstal <timovanopstal@gmail.com>
2015-03-27 16:39:44 +0100

    some typo's?

--- nrel.py
+++ nrel.py
@@ -12,7 +12,8 @@ gen = MeshGen(params)
 
 gen.load_airfoils()
 gen.resample_airfoils()
-gen.resolve_join()
+if params.length_mode != 'extruded':
+    gen.resolve_join()
 
 if params.length_mode != 'extruded' and params.mesh_mode != '2d':
     gen.resample_length()
